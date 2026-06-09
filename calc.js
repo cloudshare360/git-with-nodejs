38449ab39658ef0ea1635cda616a7ce685b71410
sri cloud share <cloud.share360@gmail.com>
2026-06-08 20:14:23 -0500

    added multiplication

--- calc.js
+++ calc.js
@@ -16,10 +16,24 @@ function subNumbers(a, b) {
     return a- b;
 }
 
+// Function to multiply two numbers
+function mulNumbers(a, b) {
+    return a * b;
+}
+
 // Example usage
 let num1 = 20;
 let num2 = 15;
 
 let result = subNumbers(num1, num2);
+
+console.log("The sum is:", result);
+
+
+// Example usage
+let num1 = 20;
+let num2 = 15;
+
+let result = mulNumbers(num1, num2);
 
 console.log("The sum is:", result);
