a9557eb5eb02345b097c9e017d1a879d6ccb5b5c
sri cloud share <cloud.share360@gmail.com>
2026-06-08 20:06:56 -0500

    adding sub for calc app

---
+++ calc.js
@@ -0,0 +1,12 @@
+// Function to add two numbers
+function subNumbers(a, b) {
+    return a- b;
+}
+
+// Example usage
+let num1 = 20;
+let num2 = 15;
+
+let result = subNumbers(num1, num2);
+
+console.log("The sum is:", result);
